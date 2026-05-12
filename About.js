2f8871d12e3d9605dc2b5ce4285f18ed0e77033e
Young lord <younglord581@gmail.com>
2026-05-12 18:06:00 +0100

    Add About.js - Company information page

---
+++ About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className="about-page">
+      <div className="container">
+        <h1>👨‍💼 About FurnitureHub</h1>
+        <div className="about-content">
+          <p>We craft beautiful furniture that transforms spaces. Partnering with top designers worldwide.</p>
+          <p>Quality materials, modern design, affordable prices. Your dream home starts here.</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default About;
