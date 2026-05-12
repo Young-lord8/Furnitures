3f3ef4dfcc209200f963123fdb6611109ede3c87
Young lord <younglord581@gmail.com>
2026-05-12 17:00:22 +0100

    Add Footer.js - Footer component

---
+++ footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>&copy; 2024 FurnitureHub. All rights reserved.</p>
+        <div className="footer-links">
+          <a href="#privacy">Privacy</a> | <a href="#terms">Terms</a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
